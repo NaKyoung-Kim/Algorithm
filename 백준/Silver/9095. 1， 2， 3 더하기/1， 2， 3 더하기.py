029612a82162938fe2ed2f0e48fa1d8afc231098
NaKyoung-Kim <78343969+NaKyoung-Kim@users.noreply.github.com>
2024-05-01 00:57:41 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -4,9 +4,9 @@ def dp(n):
     if n == 1:
         return 1
     elif n == 2:
-        return dp(n-1)+1
+        return 2
     elif n == 3:
-        return dp(n-1)+dp(n-2)+1
+        return 4
     else:
         return dp(n-3) + dp(n-2) +dp(n-1)
 

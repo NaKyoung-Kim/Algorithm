c00939d44c61be33cedb320c64b27334441e6594
NaKyoung-Kim <nkkim.ktds@gmail.com>
2025-07-07 20:55:20 +0900

    [Silver I] Title: 문자열 교환, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.java
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.java
@@ -3,39 +3,38 @@ import java.util.*;
 
 public class Main
 {
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    String str = br.readLine();
 		
-		int cntA = 0;
-		int strLen = str.length();
-        for (int i = 0; i < strLen; i++) {
-            if (str.charAt(i) == 'a') {
-                cntA++;
-            }
-        }
+	    int cntA = 0;
+	    int strLen = str.length();
+	    
+	    for (int i = 0; i < strLen; i++) {
+    		if (str.charAt(i) == 'a') {
+    		    cntA++;
+    		}
+	    }
 		
-		int minB = strLen;
-		for(int i = 0; i < strLen; i++) {
+	    int minB = strLen;
+	    for (int i = 0; i < strLen; i++) {
 		    int tempB = 0;
 		    
-		    for(int j = 0; j < cntA; j++) {
+		    for (int j = 0; j < cntA; j++) {
 		        
 		        int idx = i+j;
 		        if (idx >= strLen) {
 		            idx -= strLen;
 		        }
 		        
-		        if (str.charAt(idx) == 'b') {
-		            tempB++;   
-		        }
-
+    		    if (str.charAt(idx) == 'b') {
+    		        tempB++;   
+    		    }
 		    }
-		    if (tempB < minB) {
-	            minB = tempB;
-	        }
-		}
-		
-		System.out.println(minB);
+    		if (tempB < minB) {
+    		    minB = tempB;
+    		}
+	    }
+	    System.out.println(minB);
 	}
 }
